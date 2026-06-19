985b31d4824452e8b876213312258d2e90a90bc3
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-06-19 15:39:08 +0600

    real section complete with static data

--- src/components/home/RealPeople.jsx
+++ src/components/home/RealPeople.jsx
@@ -1,7 +1,10 @@
+import { FaQuoteLeft } from "react-icons/fa";
 import Container from "../shared/Container";
+import { Avatar } from "@heroui/react";
 
 
 const RealPeople = () => {
+
     return (
         <section className="py-20 md:py-24">
             <Container>
@@ -27,6 +30,66 @@ const RealPeople = () => {
                             <div>
                                 <h1 className="text-3xl font-bold text-foreground">90+</h1>
                                 <p className="text-muted">Countries</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-4 xl:p-6 bg-card rounded-2xl flex flex-col border border-border justify-between">
+                        <div className="space-y-4">
+                            <FaQuoteLeft className="text-green-600 text-2xl" />
+                            <h4 className="text-muted line-clamp-5">The stories here remind me that I&apos;m not alone. Every lesson pushes me to be better.</h4>
+                        </div>
+                        <div className="flex gap-3.5 items-center">
+                            <Avatar size="lg">
+                                <Avatar.Image
+                                    referrerPolicy="no-referrer"
+                                    alt={"User Name"}
+                                    src={"https://heroui-assets.nyc3.cdn.digitaloceanspaces.com/avatars/orange.jpg"}
+                                />
+                                <Avatar.Fallback delayMs={600}>JD</Avatar.Fallback>
+                            </Avatar>
+                            <div>
+                                <h3 className="text-foreground text-xl capitalize font-medium">Daniel M.</h3>
+                                <span className="text-muted text-sm">Student</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-4 xl:p-6 bg-card rounded-2xl flex flex-col border border-border justify-between">
+                        <div className="space-y-4">
+                            <FaQuoteLeft className="text-pink-600 text-2xl" />
+                            <h4 className="text-muted line-clamp-5">I&apos;ve learned more from real people&apos;s experiences than any book or course.</h4>
+                        </div>
+                        <div className="flex gap-3.5 items-center">
+                            <Avatar size="lg">
+                                <Avatar.Image
+                                    referrerPolicy="no-referrer"
+                                    alt={"User Name"}
+                                    src={"https://heroui-assets.nyc3.cdn.digitaloceanspaces.com/avatars/orange.jpg"}
+                                />
+                                <Avatar.Fallback delayMs={600}>JD</Avatar.Fallback>
+                            </Avatar>
+                            <div>
+                                <h3 className="text-foreground text-xl capitalize font-medium">Sophia L.</h3>
+                                <span className="text-muted text-sm">Marketing Manager</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-4 xl:p-6 bg-card rounded-2xl flex flex-col border border-border justify-between">
+                        <div className="space-y-4">
+                            <FaQuoteLeft className="text-blue-600 text-2xl" />
+                            <h4 className="text-muted line-clamp-5">This community is pure motivation and positive energy</h4>
+                        </div>
+                        <div className="flex gap-3.5 items-center">
+                            <Avatar size="lg">
+                                <Avatar.Image
+                                    referrerPolicy="no-referrer"
+                                    alt={"User Name"}
+                                    src={"https://heroui-assets.nyc3.cdn.digitaloceanspaces.com/avatars/orange.jpg"}
+                                />
+                                <Avatar.Fallback delayMs={600}>JD</Avatar.Fallback>
+                            </Avatar>
+                            <div>
+                                <h3 className="text-foreground text-xl capitalize font-medium">Kevin P.</h3>
+                                <span className="text-muted text-sm">Softwore Engineer</span>
                             </div>
                         </div>
                     </div>
